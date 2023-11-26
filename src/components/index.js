6ddb5e11b5ff927329af253377e3c9fa39860953
sidpd57 <sidpd57@gmail.com>
2023-11-26 23:14:56 +0530

    Refactoring navigation links for Login and SingUp components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./layouts/Header"
 import Footer from "./layouts/Footer"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
 
-
-export {Header, Footer}
+export {Header, Footer, Login, SignUp}
