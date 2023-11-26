ba8598d981c73947303eda7d4168289db6ccb106
sidpd57 <sidpd57@gmail.com>
2023-11-27 00:02:59 +0530

    update links in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,7 @@
 import Header from "./layouts/Header"
 import Footer from "./layouts/Footer"
-<<<<<<< HEAD
+import Contact from "./pages/Contact"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 
-export {Header, Footer, Login, SignUp}
-=======
-import Contact from "./pages/Contact"
-
-export { Header, Footer, Contact}
->>>>>>> main
+export { Header, Footer, Contact, Login, SignUp}
