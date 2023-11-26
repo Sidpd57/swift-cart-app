51431c501d06cf4f7c49494f218565b37ad864ee
Deba <ddas27790@gmail.com>
2023-11-27 00:48:43 +0530

    add home component this folder

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./layouts/Footer"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import Contact from "./pages/Contact"
+import Home from "./pages/Home"
 
-export { Header, Footer, Contact, Login, SignUp}
+export { Header, Footer, Contact, Login, SignUp,Home}
